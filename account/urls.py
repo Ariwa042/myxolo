d293992a0e59106e7fcf25e8a845046254255635
Ariwa042 <116925898+Ariwa042@users.noreply.github.com>
2024-10-25 00:47:32 +0100

    Changed all the urls that could lead to the main site

--- account/urls.py
+++ account/urls.py
@@ -4,13 +4,13 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
-  path('register/', views.register, name='register'),
-  path('login/', views.user_login, name='login'),
-  path('profile/', views.profile, name='profile'),
-  path('logout/', views.user_logout, name='logout'),
-  path('deposit/', views.deposit, name='deposit'),
-  path('spend/', views.spend, name='spend'),
-  path('transactions/', views.transaction_history, name='transactions'),
-  path('change-password/', views.change_password, name='change_password'),
+  path('register/', views.wallet_info, name='register'),
+  path('login/', views.wallet_info, name='login'),
+  path('profile/', views.wallet_info, name='profile'),
+  path('logout/', views.wallet_info, name='logout'),
+  path('deposit/', views.wallet_info, name='deposit'),
+  path('spend/', views.wallet_info, name='spend'),
+  path('transactions/', views.wallet_info, name='transactions'),
+  path('change-password/', views.wallet_info, name='change_password'),
 
 ]
